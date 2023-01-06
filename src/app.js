d22f5fb8cd5947810167c93f45262baacd36230b
drebraga <dre.braga@gmail.com>
2023-01-06 20:46:46 -0300

    porta 5000

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import USERS from "./USERS.js";
 import TWEETS from "./TWEETS.js";
 
 const server = express();
-const PORT = 5001;
+const PORT = 5000;
 const MAXSHOW = 10;
 const CREATED = 201;
 const BADREQUEST = 400;
